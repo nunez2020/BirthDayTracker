45704e40553e0222aafe137f0c0277329412cfa3
nunez2020 <nunez.nunez21@gmail.com>
2020-11-09 19:58:55 -0500

    Its My Birthday Tracker complete

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+require('./server/db/config');
 
 const express = require('express');
 const path = require('path');
 const app = express();
 
+const birthdayRouter = require('./server/routes/birthdays');
+
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+ if (process.env.NODE_ENV === 'production') {
+  }
 }
+
+app.use(express.json());
+app.use(birthdayRouter);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
